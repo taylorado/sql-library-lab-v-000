7fe73f87b04b4d06e5390d723e5ea72f205e5ac3
Taylor Donlan <taylordonlan@gmail.com>
2018-06-19 18:02:51 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,16 +1,16 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
-   FROM books
-   JOIN series
-   ON books.series_id = series.id
-   WHERE series.id = 1
-   ORDER BY series.id"
+     FROM books
+     JOIN series
+     ON books.series_id = series.id
+     WHERE series.id = 1
+     ORDER BY series.id"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
-   FROM characters
-   ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
+    FROM characters
+    ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
 end
 
 
@@ -23,21 +23,21 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
-  JOIN series
-  ON authors.id = series.author_id
-  JOIN subgenres
-  ON series.subgenre_id = subgenres.id"
+    FROM authors
+    JOIN series
+    ON authors.id = series.author_id
+    JOIN subgenres
+    ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series
-  JOIN characters
-  ON series.id = characters.series_id
-  WHERE characters.species = 'human'
-  GROUP BY series.title
-  ORDER BY COUNT(characters.species) DESC LIMIT 1"
+    FROM series
+    JOIN characters
+    ON series.id = characters.series_id
+    WHERE characters.species = 'human'
+    GROUP BY series.title
+    ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
